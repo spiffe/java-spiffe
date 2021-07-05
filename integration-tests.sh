b679e0213aa67e5db20cf1e92cf0a77180365fc7
Max Lambrecht <maxlambrecht@gmail.com>
2021-07-05 13:19:47 -0300

    Add some extra time to setup.
    Signed-off-by: Max Lambrecht <maxlambrecht@gmail.com>

--- integration-tests.sh
+++ integration-tests.sh
@@ -14,17 +14,17 @@ rm -rf spire-0.12.3
 curl -s -N -L https://github.com/spiffe/spire/releases/download/v0.12.3/spire-0.12.3-linux-x86_64-glibc.tar.gz | tar xz
 pushd spire-0.12.3
 bin/spire-server run -config conf/server/server.conf &
-sleep 5
+sleep 10
 
 # Run the SPIRE agent with the join token
 bin/spire-server token generate -spiffeID spiffe://example.org/myagent > token
 cut -d ' ' -f 2 token > token_stripped
 bin/spire-agent run -config conf/agent/agent.conf -joinToken "$(< token_stripped)" &
-sleep 5
+sleep 10
 
 # Register the workload through UID with the SPIFFE ID "spiffe://example.org/myservice"
 bin/spire-server entry create -parentID spiffe://example.org/myagent -spiffeID spiffe://example.org/myservice -selector unix:uid:$(id -u)
-sleep 5
+sleep 10
 popd
 
 export SPIFFE_ENDPOINT_SOCKET="unix:/tmp/agent.sock"
