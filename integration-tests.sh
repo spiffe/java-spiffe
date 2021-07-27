f9e847dba6eec42dd48904af58327f63d7fa14f7
Max Lambrecht <maxlambrecht@gmail.com>
2021-07-27 12:11:40 -0300

    Adress PR comments: improving integration-tests.sh
    Signed-off-by: Max Lambrecht <maxlambrecht@gmail.com>

--- integration-tests.sh
+++ integration-tests.sh
@@ -5,7 +5,12 @@
 
 set -euf -o pipefail
 
+export SPIFFE_ENDPOINT_SOCKET="unix:/tmp/spire-agent/public/api.sock"
+
+spire_version="1.0.0"
 spire_folder="spire-858d04b"
+spire_server_log_file="/tmp/spire-server/server.log"
+spire_agent_log_file="/tmp/spire-agent/agent.log"
 
 function cleanup() {
   killall -9 spire-agent || true
@@ -19,25 +24,54 @@ function cleanup() {
 trap cleanup EXIT
 
 # Install and run a SPIRE server
-curl -s -N -L https://github.com/spiffe/spire/releases/download/v1.0.0/spire-1.0.0-linux-x86_64-glibc.tar.gz | tar xz
+curl -s -N -L https://github.com/spiffe/spire/releases/download/v${spire_version}/spire-${spire_version}-linux-x86_64-glibc.tar.gz | tar xz
 pushd "${spire_folder}"
-bin/spire-server run -config conf/server/server.conf &
-sleep 10
+mkdir -p /tmp/spire-server
+bin/spire-server run -config conf/server/server.conf > "${spire_server_log_file}" 2>&1 &
 
-# Run the SPIRE agent with the join token
-bin/spire-server token generate -spiffeID spiffe://example.org/myagent > token
+spire_server_started=0
+for i in {1..10}
+do
+    if bin/spire-server healthcheck  >/dev/null 2>&1; then
+        spire_server_started=1
+        break
+    fi
+    sleep 1
+done
+
+if [ ${spire_server_started} -ne 1 ]; then
+    cat ${spire_server_log_file} >&2
+    echo 'SPIRE Server failed to start' >&2
+    exit 1
+fi
+
+# Generate token and run Spire Agent
+agent_id="spiffe://example.org/myagent"
+bin/spire-server token generate -spiffeID ${agent_id} > token
 cut -d ' ' -f 2 token > token_stripped
-bin/spire-agent run -config conf/agent/agent.conf -joinToken "$(< token_stripped)" &
-sleep 10
+mkdir -p /tmp/spire-agent
+bin/spire-agent run -config conf/agent/agent.conf -joinToken "$(< token_stripped)" > "${spire_agent_log_file}" 2>&1 &
+
+spire_agent_started=0
+for i in {1..10}
+do
+    if bin/spire-agent healthcheck  >/dev/null 2>&1; then
+        spire_agent_started=1
+        break
+    fi
+    sleep 1
+done
+
+if [ ${spire_agent_started} -ne 1 ]; then
+    cat ${spire_agent_log_file} >&2
+    echo 'SPIRE Agent failed to start' >&2
+    exit 1
+fi
 
 # Register the workload through UID with the SPIFFE ID "spiffe://example.org/myservice"
-bin/spire-server entry create -parentID spiffe://example.org/myagent -spiffeID spiffe://example.org/myservice -selector unix:uid:$(id -u)
-sleep 10
+bin/spire-server entry create -parentID ${agent_id} -spiffeID spiffe://example.org/myservice -selector unix:uid:$(id -u)
+sleep 10  # this value is derived from the default Agent sync interval
 popd
-
-export SPIFFE_ENDPOINT_SOCKET="unix:/tmp/spire-agent/public/api.sock"
 
 # Run only the integration tests
 ./gradlew integrationTest
-
-cleanup
