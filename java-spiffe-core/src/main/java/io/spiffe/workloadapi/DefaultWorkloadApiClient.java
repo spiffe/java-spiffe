b8137eb105c995e0beae1f72c730598ce34e6fc1
Max Lambrecht <max.lambrecht@hpe.com>
2023-01-09 17:28:51 -0300

    Fix close method in WorkloadApiClient
    Signed-off-by: Max Lambrecht <max.lambrecht@hpe.com>

--- java-spiffe-core/src/main/java/io/spiffe/workloadapi/DefaultWorkloadApiClient.java
+++ java-spiffe-core/src/main/java/io/spiffe/workloadapi/DefaultWorkloadApiClient.java
@@ -334,12 +334,12 @@ public final class DefaultWorkloadApiClient implements WorkloadApiClient {
                 for (val context : cancellableContexts) {
                     context.close();
                 }
+                retryExecutor.shutdown();
+                executorService.shutdown();
 
                 if (managedChannel != null) {
                     managedChannel.close();
                 }
-                retryExecutor.shutdown();
-                executorService.shutdown();
                 closed = true;
             }
         }
