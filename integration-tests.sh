c1c9be0c2e9711318f8901f6cd81b642b62cb856
Max Lambrecht <max.lambrecht@hpe.com>
2023-01-11 15:26:40 -0300

    Update SPIRE version for integration tests to 1.5.3 (#105)
    Signed-off-by: Max Lambrecht <max.lambrecht@hpe.com>

--- integration-tests.sh
+++ integration-tests.sh
@@ -7,7 +7,7 @@ set -euf -o pipefail
 
 export SPIFFE_ENDPOINT_SOCKET="unix:/tmp/spire-agent/public/api.sock"
 
-spire_version="1.4.0"
+spire_version="1.5.3"
 spire_folder="spire-${spire_version}"
 spire_server_log_file="/tmp/spire-server/server.log"
 spire_agent_log_file="/tmp/spire-agent/agent.log"
