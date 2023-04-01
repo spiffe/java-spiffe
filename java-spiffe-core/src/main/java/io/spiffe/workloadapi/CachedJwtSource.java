8fd2b66422e60cace0f19e0c78313d375ca6b503
Max Lambrecht <maxlambrecht@gmail.com>
2023-04-01 17:45:05 -0500

    Address PR comment
    Co-authored-by: Ryan Turner <rturner3@users.noreply.github.com>
    Signed-off-by: Max Lambrecht <max.lambrecht@hpe.com>

--- java-spiffe-core/src/main/java/io/spiffe/workloadapi/CachedJwtSource.java
+++ java-spiffe-core/src/main/java/io/spiffe/workloadapi/CachedJwtSource.java
@@ -221,7 +221,7 @@ public class CachedJwtSource implements JwtSource {
         }
     }
 
-    // Check if the jwtSvids map contains the cacheKey, returns it if it does and the JWT SVID has not past its half lifetime.
+    // Check if the jwtSvids map contains the cacheKey, returns it if it does and the JWT SVID has not passed its half lifetime.
     // If the cache does not contain the key or the JWT SVID has past its half lifetime, make a new FetchJWTSVID call to the Workload API,
     // adds the JWT SVIDs to the cache map and returns them.
     // Only one thread can fetch new JWT SVIDs and update the cache at a time.
